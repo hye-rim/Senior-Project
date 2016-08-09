63b0ca819117ff448b8e7a6bc8f1f86bd07597dd
hye-rim <phl2898@gmail.com>
2016-08-09 15:44:23 +0900

    Add word search. (네이버 백과사전 검색 api 이용 - 차후 다시 결정 필요함!)

--- Android/Onpuri2/app/src/main/java/com/onpuri/Activity/SearchFragment.java
+++ Android/Onpuri2/app/src/main/java/com/onpuri/Activity/SearchFragment.java
@@ -37,6 +37,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -50,7 +51,7 @@ public class SearchFragment extends Fragment {
     private static View view;
     private TextToSpeech tts;
     private Button btn_listen;
-    private TextView tv_sen, tv_word;
+    private TextView tv_sen, tv_word, tv_word_title;
     private ListView list;
 
     String searchText;
@@ -85,7 +86,10 @@ public class SearchFragment extends Fragment {
         tv_word = (TextView)header.findViewById(R.id.tv_word_search);
         list = (ListView)view.findViewById(R.id.list_search_sen);
         btn_listen = (Button)header.findViewById(R.id.btn_search_word_listen);
+        tv_word_title = (TextView)header.findViewById(R.id.tv_search_word);
         loadData();
+
+        tv_word_title.setText(searchText);
 
         tts = new TextToSpeech(getActivity(), new TextToSpeech.OnInitListener() {
             @Override
@@ -99,7 +103,7 @@ public class SearchFragment extends Fragment {
         btn_listen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String text = "word";
+                String text = (String) tv_word_title.getText();
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     ttsGreater21(text);
                 } else {
@@ -138,23 +142,129 @@ public class SearchFragment extends Fragment {
             }
         });
 
+        searchWord();
 
-        boolean inItem = false, inTitle = false, inAddress = false, inMapx = false, inMapy = false;
+        return view;
+    }
 
-        String title = null, address = null, mapx = null, mapy = null;
+    private void searchWord() {
+        new Thread(){
+            public void run(){
 
-        // 클라이언트 아이디 및 시크릿 그리고 요청 URL 선언
-        String clientId = "6VCLI3yWvTEYI8GqnDNO";
-        String clientSecret = "maNEdaKzXu";
+                // 클라이언트 아이디 및 시크릿 그리고 요청 URL 선언
+                String clientId = "6VCLI3yWvTEYI8GqnDNO";
+                String clientSecret = "maNEdaKzXu";
+                //encyc : 백과사전, book : 책
+                try{
+                    String text = URLEncoder.encode( searchText ,"UTF-8");
+                    String apiURL = "https://openapi.naver.com/v1/search/encyc.xml?query="+ text +"&display=1&start=1";
+                    URL url = new URL(apiURL);
 
-        try{
-            String text = URLEncoder.encode( "안녕" ,"UTF-8");
-            String apiURL = "https://openapi.naver.com/v1/search/news.xml?query="+ text +"&start=1&display=100";
-            URL url = new URL(apiURL);
+            /*
             HttpURLConnection con = (HttpURLConnection)url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("X-Naver-Client-Id", clientId);
             con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+*/
+                    URLConnection urlConn;
+                    //url 연결
+                    urlConn = url.openConnection();
+                    urlConn.setRequestProperty("X-naver-Client-Id", clientId);
+                    urlConn.setRequestProperty("X-naver-Client-Secret", clientSecret);
+
+                    //파싱 - 팩토리 만들고 팩토리로 파서 생성 (풀 파서 사용)
+                    XmlPullParserFactory factory;
+
+                    factory = XmlPullParserFactory.newInstance();
+                    XmlPullParser parser = factory.newPullParser();
+                    parser.setInput((new InputStreamReader(urlConn.getInputStream())));
+
+                    int eventType = parser.getEventType();
+                    String b = "";
+                    while (eventType != XmlPullParser.END_DOCUMENT) {
+                        switch (eventType) {
+                            case XmlPullParser.END_DOCUMENT: // 문서의 끝
+                                break;
+                            case XmlPullParser.START_DOCUMENT:
+                                b = b + " ";
+                                Log.d(TAG, b);
+                                break;
+                            case XmlPullParser.START_TAG: {
+                                Log.d(TAG, b);
+                                String tag = parser.getName();
+                                switch (tag) {
+                                    /*
+                                    case "item":
+                                        b = b + "...";
+                                        Log.d(TAG, b);
+                                        break;
+
+                                    case "title":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+                                        Log.d(TAG, b);
+                                        break;
+                                    case "link":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+                                        break;
+
+                                    case "image":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+                                        break;
+                                    case "author":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+                                        break;
+                                    case "discount":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+                                        break;
+                                    case "publisher":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+                                        break;
+                                    case "pubdate":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+                                        break;
+                                    case "isbn":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+                                        break;
+                                        */
+                                    case "description":
+                                        if (b != null)
+                                            b = b + (parser.nextText());
+
+                                        break;
+                                }
+                                break;
+                            }
+
+                            case XmlPullParser.END_TAG: {
+                                String tag = parser.getName();
+                                if (tag.equals("item")) {
+                                    b.replaceFirst("Naver", "");
+                                    b.replaceFirst("Search Result", "");
+                                    final String finalB = b;
+                                    getActivity().runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            tv_word.setText(finalB);
+                                        }
+                                    });
+                                    b = null;
+                                }
+
+                            }
+
+                        }
+                        eventType = parser.next();
+                    }
+
+/*
             // response 수신
             int responseCode = con.getResponseCode();
             if (responseCode == 200) {
@@ -170,12 +280,22 @@ public class SearchFragment extends Fragment {
             } else {
                 tv_word.setText("API 호출 에러 발생 : 에러코드=" + responseCode);
             }
-
-        } catch(Exception e){
-            tv_word.setText("구현 예정입니다");
-        }
-
-        return view;
+            */
+                } catch (MalformedURLException e) {
+                    Log.e(TAG, "MalformedURLException");
+                    tv_word.setText("구현 예정입니다");
+                } catch (UnsupportedEncodingException e) {
+                    Log.e(TAG, "UnsupportedEncodingException");
+                    tv_word.setText("구현 예정입니다");
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException");
+                    tv_word.setText("구현 예정입니다");
+                } catch (XmlPullParserException e) {
+                    Log.e(TAG, "XmlPullParserException");
+                    tv_word.setText("구현 예정입니다");
+                }
+            }
+        }.start();
     }
 
     public void loadData(){
