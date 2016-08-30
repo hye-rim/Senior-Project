4339defd048ba87750604ec5a5a10e4b531acfdf
hye-rim <phl2898@gmail.com>
2016-08-30 19:41:12 +0900

    Add permission "internet".

--- Android/Onpuri2/app/src/main/java/com/onpuri/Activity/SplashActivity.java
+++ Android/Onpuri2/app/src/main/java/com/onpuri/Activity/SplashActivity.java
@@ -140,6 +140,8 @@ public class SplashActivity extends Activity {
             permissionsNeeded.add("WRITE_STORAGE");
         if (!addPermission(permissionsList,Manifest.permission.READ_EXTERNAL_STORAGE))
             permissionsNeeded.add("READ_STORAGE");
+        if(!addPermission(permissionsList,Manifest.permission.INTERNET))
+            permissionsNeeded.add("INTERNET");
 
         if (permissionsList.size() > 0) {
             if (permissionsNeeded.size() > 0) {
@@ -191,6 +193,7 @@ public class SplashActivity extends Activity {
                 perms.put(Manifest.permission.READ_PHONE_STATE, PackageManager.PERMISSION_GRANTED);
                 perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE,PackageManager.PERMISSION_GRANTED);
                 perms.put(Manifest.permission.READ_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.INTERNET, PackageManager.PERMISSION_GRANTED);
 
                 // Fill with results
                 for (int i = 0; i < permissions.length; i++)
@@ -199,6 +202,7 @@ public class SplashActivity extends Activity {
                 if (perms.get(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED
                         && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
                         && perms.get(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED
                         ) {
                     // All Permissions Granted
                     splashWork();
